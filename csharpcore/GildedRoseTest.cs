86cdfc81b2ce7cbdee96595fd38863044e41be1d
davidadsit <david.adsit@gmail.com>
2021-02-03 21:37:36 -0700

    Implement conjured

--- csharpcore/GildedRoseTest.cs
+++ csharpcore/GildedRoseTest.cs
@@ -16,6 +16,18 @@ namespace csharpcore
 
             Assert.Equal("foo", items[0].Name);
         }
+        
+        [Fact]
+        public void Conjured_items_change_quality_twice_as_fast()
+        {
+            var items = new List<Item> {new Item {Name = "Conjured foo", SellIn = 5, Quality = 8}};
+
+            var app = new GildedRose(items);
+            app.UpdateQuality();
+
+            Assert.Equal(4, items[0].SellIn);
+            Assert.Equal(6, items[0].Quality);
+        }
 
         [Fact]
         public void UpdateItem_reduces_SellIn_and_Quality()

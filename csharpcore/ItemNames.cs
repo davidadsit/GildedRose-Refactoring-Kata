48147e17489d8d15b2aa7427a2ce3b592ce013fc
davidadsit <david.adsit@gmail.com>
2021-02-04 11:30:19 -0700

    Only used consts for exceptional items

--- csharpcore/ItemNames.cs
+++ csharpcore/ItemNames.cs
@@ -4,9 +4,6 @@ namespace csharpcore
     {
         public const string ConcertPasses = "Backstage passes to a TAFKAL80ETC concert";
         public const string Sulfuras = "Sulfuras, Hand of Ragnaros";
-        public const string Vest = "+5 Dexterity Vest";
         public const string Brie = "Aged Brie";
-        public const string Elixir = "Elixir of the Mongoose";
-        public const string Cake = "Conjured Mana Cake";
     }
 }
--- csharpcore/Program.cs
+++ csharpcore/Program.cs
@@ -11,16 +11,16 @@ namespace csharpcore
 
             var items = new List<Item>
             {
-                new Item {Name = ItemNames.Vest, SellIn = 10, Quality = 20},
+                new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
                 new Item {Name = ItemNames.Brie, SellIn = 2, Quality = 0},
-                new Item {Name = ItemNames.Elixir, SellIn = 5, Quality = 7},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
                 new Item {Name = ItemNames.Sulfuras, SellIn = 0, Quality = 80},
                 new Item {Name = ItemNames.Sulfuras, SellIn = -1, Quality = 80},
                 new Item {Name = ItemNames.ConcertPasses, SellIn = 15, Quality = 20},
                 new Item {Name = ItemNames.ConcertPasses, SellIn = 10, Quality = 49},
                 new Item {Name = ItemNames.ConcertPasses, SellIn = 5, Quality = 49},
                 // this conjured item does not work properly yet
-                new Item {Name = ItemNames.Cake, SellIn = 3, Quality = 6}
+                new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
             };
 
             var app = new GildedRose(items);
